fdab6ec291454146007b17a48f62384ce522af69
ossasoeun9 <ossasoeun9@gmail.com>
2025-01-26 02:56:34 +0700

    fix

--- models/index.js
+++ models/index.js
@@ -5,7 +5,6 @@ const config = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
   logging: process.env.LOGGING,
 };

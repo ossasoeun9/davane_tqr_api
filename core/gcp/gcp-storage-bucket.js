840422c2b75603d0be0500376079abe55a326ceb
ossasoeun9 <ossasoeun9@gmail.com>
2025-01-25 23:51:47 +0700

    update

--- core/gcp/gcp-storage-bucket.js
+++ core/gcp/gcp-storage-bucket.js
@@ -1,7 +1,7 @@
 const { Storage } = require('@google-cloud/storage');
 
 const projectId = process.env.GCP_PROJECT_ID;
-const keyFilename = 'config/davane-tqr-dev-bucket-key.json';
+const keyFilename = 'davane-tqr-dev-448615-b87f3d52cc45';
 
 const bucket = new Storage({ projectId, keyFilename }).bucket("davane_tqr");
 

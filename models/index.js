a11e4ef22ada432562bc8c66a8bcc806bf0fd3bb
ossasoeun9 <ossasoeun9@gmail.com>
2025-01-26 12:04:34 +0700

    Udpdate

--- models/index.js
+++ models/index.js
@@ -5,8 +5,7 @@ const config = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT, 
+  socketPath: process.env.DB_SOCKET_PATH,
   dialect: process.env.DB_DIALECT,
   logging: process.env.LOGGING,
 };

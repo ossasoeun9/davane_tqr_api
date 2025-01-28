b5a07001a302249d9330545a00f2f75d961b1d16
ossasoeun9 <ossasoeun9@gmail.com>
2025-01-28 23:07:56 +0700

    udpate

--- core/gcp/upload-buffer.js
+++ core/gcp/upload-buffer.js
@@ -15,8 +15,7 @@ export default async function uploadBuffer(buffer, contentType, path) {
         stream.on('error', reject);
         stream.on('finish', resolve);
       });
-
-      console.log("okkkkkkk bucket")
+      
       return true
     } catch (error) {
       console.log(`Bucket error: ${error}`)

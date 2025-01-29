3d92bc9706f41b6a394b65e58805986cb4aff99a
ossasoeun9 <ossasoeun9@gmail.com>
2025-01-30 00:28:08 +0700

    ssd

--- core/gcp/gcp-storage-bucket.js
+++ core/gcp/gcp-storage-bucket.js
@@ -5,7 +5,7 @@ const credentials = {
   type: process.env.GCP_SERVICE_TYPE,
   project_id: process.env.GCP_PROJECT_ID,
   private_key_id: process.env.GCP_PRIVATE_KEY_ID,
-  private_key: process.env.GCP_PRIVATE_KEY,
+  private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.GCP_CLIENT_EMAIL,
   client_id: process.env.GPC_CLIENT_ID,
   auth_uri: process.env.GPC_AUTH_URI,
